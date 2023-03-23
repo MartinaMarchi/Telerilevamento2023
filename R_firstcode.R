f8df5fa4f6266bcaa56e8bfff6d7ebc176c5ab56
MartinaMarchi <127427326+MartinaMarchi@users.noreply.github.com>
2023-03-23 17:37:46 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -42,12 +42,43 @@ par(mfrow=c(2,1))
 plotRGB(l2011, r=3, g=2, b=1, stretch="lin")
 plotRGB(l2011, r=3, g=4, b=2, stretch="hist")
 
-# Exercise: upload the image from 1988
-l1988 <- brick("p224r63_1988.grd")
-
 par(mfrow=c(2,1))
 plotRGB(l1988, r=4, g=3, b=2, stretch="lin")
 plotRGB(l2011, r=4, g=3, b=2, stretch="lin")
+
+#Exercise: plot the NIR band
+plot(l2011[[4]])
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin") #colori naturali
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin") #componente rossa
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin") #componente verde
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin") #infrarosso montato su componente blu
+
+#Import the 1988 image
+l1988 <- brick("p224r63_1988_masked.grd")
+plot(l1988)
+#Plot in RGB space (natural colours)
+plotRGB(l1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l1988, 4, 3, 2, stretch="Lin")
+
+#multiframe
+par(mfrow=c(2,1))
+plotRGB(l1988, 4, 3, 2, stretch="Lin")
+plotRGB(l2011, 4, 3, 2, stretch="Lin")
+
+#hist
+plotRGB(l2011, 4, 3, 2, stretch="Hist")
+
+#multiframe with 4 images
+par(mfrow=c(2,2))
+plotRGB(l1988, r=3, g=4, b=2, stretch="Lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
+plotRGB(l1988, r=3, g=4, b=2, stretch="Hist")
+plotRGB(l2011, r=3, g=4, b=2, stretch="Hist")
+
+
+
 
 
 
