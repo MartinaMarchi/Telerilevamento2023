16a1859c12aa9a314b2fa3118f81cc9db8a8b81f
MartinaMarchi <127427326+MartinaMarchi@users.noreply.github.com>
2024-08-19 09:06:21 +0200

    Update R_Copernicus.R

--- R_Copernicus.R
+++ R_Copernicus.R
@@ -21,3 +21,19 @@ ggplot() +
 ggplot() +
 geom_raster(ssoil, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
 scale_fill_viridis(option="magma")
+
+# R code for downloading and visualizing Copernicus data
+
+# 1- Packages
+# install.packages("ncdf4")
+library(ncdf4)
+library(raster)
+
+# 2- Working directory
+setwd("C:/lab/")
+
+# 3- Importing imagery
+sc <- raster("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
+
+# 4- Plotting the image
+plot(sc)
